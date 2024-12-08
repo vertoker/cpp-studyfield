405f627055e33909db962d2db755a0008390c643
vertoker <vertogpro@gmail.com>
2024-12-08 11:39:00 +0500

    end 2.1

--- 1-cpp-primer/README.md
+++ 1-cpp-primer/README.md
@@ -1,6 +1,6 @@
 ## C++ Primer Fifth Edition
 
-![](../docs/1-cpp-primer.png)
+![](../docs/1-cpp-primer/preview.png)
 
 - https://www.amazon.com/Primer-5th-Stanley-B-Lippman/dp/0321714113
 - https://ozon.ru/t/OR3E6o7
---
+++ 1-cpp-primer/ch02/ex01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    // logic types
+    std::cout << "bool: " << sizeof(bool) << " bytes, " << sizeof(bool) * 8 << " bits" << std::endl;
+
+    // "string" integer types
+    std::cout << "char: " << sizeof(char) << " bytes, " << sizeof(char) * 8 << " bits" << std::endl; // also byte
+    std::cout << "wchar_t: " << sizeof(wchar_t) << " bytes, " << sizeof(wchar_t) * 8 << " bits" << std::endl;
+    std::cout << "char16_t: " << sizeof(char16_t) << " bytes, " << sizeof(char16_t) * 8 << " bits" << std::endl;
+    std::cout << "char32_t: " << sizeof(char32_t) << " bytes, " << sizeof(char32_t) * 8 << " bits" << std::endl;
+
+    // integer types
+    std::cout << "short: " << sizeof(short) << " bytes, " << sizeof(short) * 8 << " bits" << std::endl;
+    std::cout << "int: " << sizeof(int) << " bytes, " << sizeof(int) * 8 << " bits" << std::endl;
+    std::cout << "long: " << sizeof(long) << " bytes, " << sizeof(long) * 8 << " bits" << std::endl;
+    std::cout << "long long: " << sizeof(long long) << " bytes, " << sizeof(long long) * 8 << " bits" << std::endl;
+    
+    // floating point number types
+    std::cout << "float: " << sizeof(float) << " bytes, " << sizeof(float) * 8 << " bits" << std::endl;
+    std::cout << "double: " << sizeof(double) << " bytes, " << sizeof(double) * 8 << " bits" << std::endl;
+    std::cout << "long double: " << sizeof(long double) << " bytes, " << sizeof(long double) * 8 << " bits" << std::endl;
+
+    // `void` means nothing, then it has no size (in computer meaning)
+}
