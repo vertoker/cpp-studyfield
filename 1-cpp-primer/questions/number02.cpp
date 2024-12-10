66815ad92fcb20c3ce3bfa99c69bdfa9c6183680
vertoker <vertogpro@gmail.com>
2024-12-10 23:21:17 +0500

    joker hihihaha

---
+++ 1-cpp-primer/questions/number02.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+    int i = 42;
+    int* pi = &i;
+    int** ppi = &pi;
+    int*** pppi = &ppi;
+    int**** ppppi = &pppi;
+    int***** pppppi = &ppppi;
+    int****** ppppppi = &pppppi;
+    int******* pppppppi = &ppppppi;
+    int******** ppppppppi = &pppppppi;
+    int********* pppppppppi = &ppppppppi;
+    int********** ppppppppppi = &pppppppppi;
+    int*********** pppppppppppi = &ppppppppppi;
+    int************ ppppppppppppi = &pppppppppppi;
+    int************* pppppppppppppi = &ppppppppppppi;
+    int************** ppppppppppppppi = &pppppppppppppi;
+    int*************** pppppppppppppppi = &ppppppppppppppi;
+    int**************** ppppppppppppppppi = &pppppppppppppppi;
+    int***************** pppppppppppppppppi = &ppppppppppppppppi;
+    int****************** ppppppppppppppppppi = &pppppppppppppppppi;
+    int******************* pppppppppppppppppppi = &ppppppppppppppppppi;
+    int******************** ppppppppppppppppppppi = &pppppppppppppppppppi;
+    int********************* pppppppppppppppppppppi = &ppppppppppppppppppppi;
+    int********************** ppppppppppppppppppppppi = &pppppppppppppppppppppi;
+    int*********************** pppppppppppppppppppppppi = &ppppppppppppppppppppppi;
+    int************************ ppppppppppppppppppppppppi = &pppppppppppppppppppppppi;
+    int************************* pppppppppppppppppppppppppi = &ppppppppppppppppppppppppi;
+    int************************** ppppppppppppppppppppppppppi = &pppppppppppppppppppppppppi;
+  //int*how*tf*does*it*even*exist* pppppppppppppppppppppppppppi = &ppppppppppppppppppppppppppi;
+    
+    std::cout << **************************ppppppppppppppppppppppppppi << std::endl; // 42
+    return 0;
+}
