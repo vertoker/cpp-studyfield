9fb3fe1ddd1b638666e12b28d56285d0ba01ec8c
vertoker <vertogpro@gmail.com>
2024-12-14 23:00:29 +0500

    start 3.2

--- 1-cpp-primer/ch03/ex01.cpp
+++ 1-cpp-primer/ch03/ex01.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+
+// I'm too lazy to rewrite it
+// I already understand how to use it
+
+// case 1: not type using
+using std::cin;
+using std::cout;
+
+// case 2: type using
+using str = std::string;
 
 int main()
 {
+    str s;
+    cin >> s;
+    std::cout << s;
     return 0;
 }
