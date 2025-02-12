3f4e6c13af68dae66b1a7ae850879a0a5544d277
vertoker <vertogpro@gmail.com>
2025-02-12 22:55:51 +0500

    save

--- 1-cpp-primer/ch12/ex17.cpp
+++ 1-cpp-primer/ch12/ex17.cpp
@@ -1,28 +1,7 @@
-/***************************************************************************
- *  @file       The code is for the exercises in C++ Primmer 5th Edition
- *  @author     Alan.W
- *  @date       24  DEC 2013
- *  @remark
- ***************************************************************************/
-//
-// Exercise 12.17:
-// Which of the following unique_ptr declarations are illegal or likely to
-// result in subsequent program error? Explain what the problem is with each
-// one.
-//
-// Exercise 12.18:
-// Why doesn’t shared_ptr have a release member?
-//  Because other shared_ptr that points the same object can still delete this
-//  object.Thus, it's meaningless to provide this member
-//  more detail can be found a thread on Stack Overflow:
-//  http://stackoverflow.com/questions/1525764/how-to-release-pointer-from-boostshared-ptr
-
-
 #include <iostream>
 #include <vector>
 #include <string>
 #include <memory>
-
 
 int main()
 {
