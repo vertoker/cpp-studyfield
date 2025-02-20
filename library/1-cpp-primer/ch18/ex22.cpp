c4db7bd0fe6b40f7d52a28e2717d8f5fc758e4f4
vertoker <vertogpro@gmail.com>
2025-02-20 21:04:11 +0500

    done ch18

---
+++ library/1-cpp-primer/ch18/ex22.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class A {};
+class B : public A{};
+class C : public B{};
+
+class X {};
+class Y {};
+class Z : public X, public Y {};
+
+class MI : public C, public Z {};
+class D : public X, public C{};
+
+// In order A, B, C, X, Y, Z, MI, D or apeearance order
+
+MI mi;
+
+int main()
+{
+    D* pd = new D;
+    X* px = pd;
+    B* pb = pd;
+    A* pa = pd;
+    C* pc = pd;
+
+    return 0;
+}
