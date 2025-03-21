6392709d64cdb3f6f52793360e64d4e74c7321e3
Kostya <vertogpro@gmail.com>
2025-03-21 17:09:22 +0500

    make rule09

--- library/2-effective-cpp/rule09.cpp
+++ library/2-effective-cpp/rule09.cpp
@@ -1,13 +1,34 @@
 // 9. Never call virtual functions during construction or destruction
 
+#include <iostream>
+
 class Base
 {
 public:
-    Base() { }
-    virtual ~Base() { }
+    Base() { f1(); }
+    virtual ~Base() { f2(); }
+
+    virtual void f1() { std::cout << "f1 base" << std::endl; }
+    virtual void f2() { std::cout << "f2 base" << std::endl; }
+    virtual void f3() { std::cout << "f3 base" << std::endl; }
+    virtual void f4() { std::cout << "f4 base" << std::endl; }
 };
 
 class Derived : public Base
 {
-    
+public:
+    Derived() : Base() { f3(); }
+    ~Derived() { f4(); }
+
+    void f1() override { std::cout << "f1 derived" << std::endl; }
+    void f2() override { std::cout << "f2 derived" << std::endl; }
+    void f3() override { std::cout << "f3 derived" << std::endl; }
+    void f4() override { std::cout << "f4 derived" << std::endl; }
+};
+
+int main()
+{
+    Derived derived; // f1 and f2 called only Base:: versions
+
+    return 0;
 }
